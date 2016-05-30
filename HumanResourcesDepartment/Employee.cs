247e630ece6f5b92bc93f85e3cc197037ac96112
Viktor Blovytskyi <leito3101@gmail.com>
2016-05-31 00:01:52 +0300

    Fixed method DisplayData().

--- HumanResourcesDepartment/Employee.cs
+++ HumanResourcesDepartment/Employee.cs
@@ -32,10 +32,10 @@ namespace HumanResourcesDepartment
         /// <summary>
         /// This method set subdivision in class employee.
         /// </summary>
-        /// <param name="sd">Subdivision object</param>
-        public void SetSubdivision(Subdivision sd)
+        /// <param name="subdivision">Subdivision object</param>
+        public void SetSubdivision(Subdivision subdivision)
         {
-            this.Subdivision = sd;
+            this.Subdivision = subdivision;
         }
 
         /// <summary>
@@ -50,16 +50,33 @@ namespace HumanResourcesDepartment
         /// <summary>
         /// This method changes employee's subdivision.
         /// </summary>
-        /// <param name="oldSubdivision">Old subdivision object</param>
-        /// <param name="newSubdivision">New subdivision object</param>
-        public void ChangeSubdivision(Subdivision newSubdivision)
+        /// <param name="subdivision">New subdivision object</param>
+        public void ChangeSubdivision(Subdivision subdivision)
         {
             this.Subdivision.RemoveEmployee(this);
-            newSubdivision.AddEmployee(this);
+            subdivision.AddEmployee(this);
         }
 
         /// <summary>
-        /// This method display data.
+        /// This methid change employee's contact information. 
+        /// </summary>
+        /// <param name="contactDetails">String with new contact details</param>
+        public void ChangeContactDetails(string contactDetails)
+        {
+            this.ContactDetails = contactDetails;
+        }
+
+        /// <summary>
+        /// This method change employee's position.
+        /// </summary>
+        /// <param name="position">String with new position</param>
+        public void ChangePosition(string position)
+        {
+            this.Position = position;
+        }
+
+        /// <summary>
+        /// This method display data. TODO delete. 
         /// </summary>
         public void DisplayData()
         {
@@ -74,6 +91,10 @@ namespace HumanResourcesDepartment
             else if(this.Subdivision == null && this.Employer != null)
             {
                 Console.WriteLine(this.FirstName + "\t" + this.LastName + "\t" + this.Position + "\t" + " " + "\t" + this.Employer.FirstName);
+            }
+            else
+            {
+                Console.WriteLine(this.FirstName + "\t" + this.LastName + "\t" + this.Position + "\t" + " " + "\t" + " ");
             }
             
         }
