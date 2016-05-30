32b74715a6839a4677ef4d00175edeb20c8752d3
Viktor Blovytskyi <leito3101@gmail.com>
2016-05-30 18:05:55 +0300

    Initial commit

---
+++ HumanResourcesDepartment/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanResourcesDepartment
+{
+    public class Class1
+    {
+        public static void main()
+        {
+            Console.WriteLine("dasdas");
+        }
+    }
+}
