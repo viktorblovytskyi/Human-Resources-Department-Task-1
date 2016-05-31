0385b355980af76513190643c4c484bcfdcc36cf
Viktor Blovytskyi <leito3101@gmail.com>
2016-05-31 14:05:42 +0300

    Hotfix.

--- HumanResourcesDepartment/Person.cs
+++ HumanResourcesDepartment/Person.cs
@@ -6,6 +6,12 @@
         public string LastName { get; protected set; }
         public string ContactDetails { get; protected set; }
 
+        /// <summary>
+        /// Initial constructor
+        /// </summary>
+        /// <param name="FirstName">Firstname of employee. Type: string. </param>
+        /// <param name="LastName">Lastname of employee. Type: string. </param>
+        /// <param name="ContactDetails">Contact details of employee. Type: string. </param>
         public Person(string FirstName, string LastName, string ContactDetails)
         {
             this.FirstName = FirstName;
